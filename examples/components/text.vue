0e043fd187d6b7d11231fb8d5d88b1ba389fa9f0
yufeng <2544772921@qq.com>
2020-06-13 17:46:51 +0800

    基础架子

---
+++ examples/components/text.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <Button type="primary">
+      测试文档
+    </Button>
+  
+  </div>
+</template>
+
+<script>
+import { Button } from 'element-ui'
+import 'element-ui/lib/theme-chalk/button.css'
+export default {
+  name: 'Text',
+  components: { Button },
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
